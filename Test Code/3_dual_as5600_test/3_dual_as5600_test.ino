12f1ead36f02f9cca90cc8dda8737dab58c07fec
makerbase-motor <38064527+makerbase-motor@users.noreply.github.com>
2024-11-19 15:06:55 +0800

    Add files via upload

--- Test Code/3_dual_as5600_test/3_dual_as5600_test.ino
+++ Test Code/3_dual_as5600_test/3_dual_as5600_test.ino
@@ -1,9 +1,5 @@
-// MKS ESP32 FOC AS5600 Text Example; Text Hardware：MKS ESP32 FOC V1.0
-// Turn the motor manually, and you can receive the motor position displayed on the serial monitor
-// The first and second columns are the position coordinates of the M0 and M1 motors respectively
-
+// MKS ESP32 FOC V2.0 | AS5600 Encoder Example | Library:SimpleFOC 2.2.1 | Hardware:MKS ESP32 FOC V2.0 & MKS AS5600
 #include <SimpleFOC.h>
-
 
 MagneticSensorI2C sensor0 = MagneticSensorI2C(AS5600_I2C);
 MagneticSensorI2C sensor1 = MagneticSensorI2C(AS5600_I2C);
@@ -12,27 +8,20 @@ TwoWire I2Ctwo = TwoWire(1);
 
 
 void setup() {
-
-  
   Serial.begin(115200);
-  _delay(750);
-  I2Cone.begin(19,18, 400000);   //SDA0,SCL0
-  I2Ctwo.begin(23,5, 400000);
-  
-  //For the latest version ESP-Arduino 2.0.2, use the following two lines of code
-  //I2Cone.begin(19,18, 400000UL);   //SDA0,SCL0
-  //I2Ctwo.begin(23,5, 400000UL);
+
+  I2Cone.begin(19, 18, 400000UL); // AS5600_M0
+  I2Ctwo.begin(23, 5, 400000UL); // AS5600_M1
   
   sensor0.init(&I2Cone);
   sensor1.init(&I2Ctwo);
 }
 
 void loop() {
-  // sensor0.update(); // If the simplefoc library version is 2.20 and above, uncomment these two lines
-  // sensor1.update();
-  //_delay(200);
-  Serial.print(sensor0.getAngle()); 
-  Serial.print(" - "); 
-  Serial.print(sensor1.getAngle());
+  sensor0.update();  // If the simplefoc library version is 2.20 and above, uncomment these two lines
+  sensor1.update();
+  Serial.print(sensor0.getAngle());
+  Serial.print(" - ");
+  Serial.println(sensor1.getAngle());
   Serial.println();
 }
